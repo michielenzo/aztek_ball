9639d5400620075ec653162c1b5891604dd077fd
Michiel Idema <m.idema@textinfo.nl>
2020-02-14 14:34:55 +0100

    added ball and camera with movement and jumping

---
+++ Aztek_Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 _offset;
+    
+    public void Start()
+    {
+        player = GameObject.Find("Player");
+        _offset = transform.position - player.transform.position;
+    }
+    
+    public void LateUpdate()
+    {
+        Relocate();
+    }
+
+    private void Relocate()
+    {
+        transform.position = player.transform.position + _offset;
+    }
+}
---
+++ Aztek_Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    
+    public float speed;
+    public float jumpForce;
+    private bool canJump = false;
+
+    public void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        if (!canJump) return;
+        canJump = false;
+        _rigidBody.AddForce(Vector3.up * 1000);
+    }
+
+    public void Update()
+    {
+        Movement();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown("space")){
+            //Debug.Log("Jump!");
+            canJump = true;
+        }
+    }
+
+    private void Movement()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        _rigidBody.AddForce(movement * speed);
+    }
+}
